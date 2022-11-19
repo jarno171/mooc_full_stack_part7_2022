0a97f156adbd910e077dcc13f23ba0bf6d4d6aeb
Jarno Varis <jarno.varis@gmail.com>
2022-11-19 12:17:46 +0200

    implemented view for showing single anecdotes, and anecdotes are shown as urls on the main page

--- routed-anecdotes/src/App.js
+++ routed-anecdotes/src/App.js
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import {
-  BrowserRouter as Router,
-  Routes, Route, Link
+  Routes, Route, Link,
+  useMatch,
 } from "react-router-dom"
 
 const Menu = () => {
@@ -21,10 +21,27 @@ const AnecdoteList = ({ anecdotes }) => (
   <div>
     <h2>Anecdotes</h2>
     <ul>
-      {anecdotes.map(anecdote => <li key={anecdote.id} >{anecdote.content}</li>)}
+      {anecdotes.map(anecdote =>
+        <li key={anecdote.id} >
+          <Link to={`/anecdotes/${anecdote.id}`}>{anecdote.content}</Link>
+        </li>
+      )}
     </ul>
   </div>
 )
+
+const Anecdote = ({ anecdote }) => {
+  return (
+    <>
+      <h2>
+        {anecdote.content}
+      </h2>
+      <p>has {anecdote.votes} votes</p>
+
+      <p>for more info see <a href={anecdote.info}>{anecdote.info}</a></p>
+    </>
+  )
+}
 
 const About = () => (
   <div>
@@ -126,18 +143,23 @@ const App = () => {
     setAnecdotes(anecdotes.map(a => a.id === id ? voted : a))
   }
 
+  const match = useMatch('/anecdotes/:id')
+  const anecdote = match 
+    ? anecdotes.find(anecdote => anecdote.id === Number(match.params.id))
+    : null
+
   return (
     <>
       <h1>Software anecdotes</h1>
 
-      <Router>
+      
           <Menu />
           <Routes>
             <Route path="/about" element={<About />} />
             <Route path="/create" element={ <CreateNew addNew={addNew} />} />
+            <Route path="/anecdotes/:id" element={<Anecdote anecdote={anecdote} />} />
             <Route path="/" element={<AnecdoteList anecdotes={anecdotes} />} />
           </Routes>
-      </Router>
 
       <Footer />
     </>
