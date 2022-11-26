686c605a41847c975c0cdc3ea68ff9d30d88e734
Jarno Varis <jarno.varis@gmail.com>
2022-11-26 10:47:03 +0200

    fixed really dumb mistake in table structure

--- frontend/src/components/BlogList.js
+++ frontend/src/components/BlogList.js
@@ -7,16 +7,16 @@ const BlogList = (props) => {
   if (props.blogs.length !== 0) {
     return (
       <>
-        <Table>
-          {props.blogs.map((blog) => (
-            <tbody key={blog.id}>
-              <tr>
+        <Table striped bordered>
+          <tbody>
+            {props.blogs.map((blog) => (
+              <tr key={blog.id}>
                 <td>
                   <Link to={`/blogs/${blog.id}`}>{blog.title}</Link>
                 </td>
               </tr>
-            </tbody>
-          ))}
+            ))}
+          </tbody>
         </Table>
       </>
     )
