77c55458dd275f84ba36661fd3939135606c27b5
Jarno Varis <jarno.varis@gmail.com>
2022-11-27 10:16:43 +0200

    added navbar too, seems nice, but could tweak the colors

--- frontend/src/components/NavigationBar.js
+++ frontend/src/components/NavigationBar.js
@@ -1,6 +1,7 @@
 import { setUser, } from '../reducers/userReducer'
 import { connect, } from 'react-redux'
 import { Link, } from "react-router-dom"
+import { Nav, Navbar } from 'react-bootstrap'
 
 const handleLogout = (setUser) => {
   window.localStorage.removeItem('loggedBlogappUser')
@@ -16,13 +17,25 @@ const LogoutBar = (props) => {
   return (
     <>
       <h1>blogs</h1>
-      <p>
-        <Link style={padding} to="/">blogs</Link>
-        <Link style={padding} to="/users">users</Link>
 
-        {props.user.name} logged in
-        <button style={padding} onClick={() => handleLogout(props.setUser)}>logout</button>
-      </p>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+         <Nav className="me-auto">
+          <Nav.Link href="#" as="span">
+            <Link style={padding} to="/">blogs</Link>
+          </Nav.Link>
+
+          <Nav.Link href="#" as="span">
+            <Link style={padding} to="/users">users</Link>
+          </Nav.Link>
+
+          <Nav.Link href="#" as="span">
+            <button style={padding} onClick={() => handleLogout(props.setUser)}>logout</button>
+          </Nav.Link>
+        </Nav>
+       </Navbar.Collapse>
+      </Navbar>
     </>
   )
 }
